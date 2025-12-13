e1dd1a0372af59ca2f84f63f1a4764e0c308074a
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-13 11:00:04 +0530

    Add blog page design in figma

--- assets/index-BbLyTrtI.js
+++ assets/index-BbLyTrtI.js
@@ -1,1 +1,70 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&r(c)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const u=document.querySelector("#toggle-menu"),a=document.querySelector("#nav-links");document.querySelector(".close-menu");u.addEventListener("click",function(){a.classList.toggle("active")});const i=document.getElementById("form"),n=document.getElementById("result");i.addEventListener("submit",function(l){l.preventDefault();const o=new FormData(i),s=Object.fromEntries(o),r=JSON.stringify(s);n.innerHTML="Please wait...",fetch("https://api.web3forms.com/submit",{method:"POST",headers:{"Content-Type":"application/json",Accept:"application/json"},body:r}).then(async e=>{let t=await e.json();e.status==200?n.innerHTML="Form submitted successfully":(console.log(e),n.innerHTML=t.message)}).catch(e=>{console.log(e),n.innerHTML="Something went wrong!"}).then(function(){i.reset(),setTimeout(()=>{n.style.display="none"},3e3)})});
+/** @format */
+
+(function () {
+  const o = document.createElement('link').relList;
+  if (o && o.supports && o.supports('modulepreload')) return;
+  for (const e of document.querySelectorAll('link[rel="modulepreload"]')) r(e);
+  new MutationObserver((e) => {
+    for (const t of e)
+      if (t.type === 'childList')
+        for (const c of t.addedNodes)
+          c.tagName === 'LINK' && c.rel === 'modulepreload' && r(c);
+  }).observe(document, { childList: !0, subtree: !0 });
+  function s(e) {
+    const t = {};
+    return (
+      e.integrity && (t.integrity = e.integrity),
+      e.referrerPolicy && (t.referrerPolicy = e.referrerPolicy),
+      e.crossOrigin === 'use-credentials'
+        ? (t.credentials = 'include')
+        : e.crossOrigin === 'anonymous'
+        ? (t.credentials = 'omit')
+        : (t.credentials = 'same-origin'),
+      t
+    );
+  }
+  function r(e) {
+    if (e.ep) return;
+    e.ep = !0;
+    const t = s(e);
+    fetch(e.href, t);
+  }
+})();
+const u = document.querySelector('#toggle-menu'),
+  a = document.querySelector('#nav-links');
+document.querySelector('.close-menu');
+u.addEventListener('click', function () {
+  a.classList.toggle('active');
+});
+const i = document.getElementById('form'),
+  n = document.getElementById('result');
+i.addEventListener('submit', function (l) {
+  l.preventDefault();
+  const o = new FormData(i),
+    s = Object.fromEntries(o),
+    r = JSON.stringify(s);
+  (n.innerHTML = 'Please wait...'),
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: r,
+    })
+      .then(async (e) => {
+        let t = await e.json();
+        e.status == 200
+          ? (n.innerHTML = 'Form submitted successfully')
+          : (console.log(e), (n.innerHTML = t.message));
+      })
+      .catch((e) => {
+        console.log(e), (n.innerHTML = 'Something went wrong!');
+      })
+      .then(function () {
+        i.reset(),
+          setTimeout(() => {
+            n.style.display = 'none';
+          }, 3e3);
+      });
+});
